865c50e2faa813883974043bb2e22bde953f167c
aditya136 <adityabanerjee102@gmail.com>
2022-10-16 10:20:14 -0700

    simple car game

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+running = True
+
+screen = pygame.display.set_mode((800, 800))
+
+pygame.display.set_caption("car game made by @aditya1366")
+screen.fill((60, 220, 0))
+
+
+pygame.quit()
